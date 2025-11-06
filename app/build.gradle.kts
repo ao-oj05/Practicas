6a4eeb200aa6ae0fcff06d92baddc2bf78c7c3ad
ao-oj05 <aliciaocana49@gmail.com>
2025-11-05 23:27:40 -0600

    Agregado el proyecto Cuartitos_App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,7 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.navigation.compose)
+    implementation("androidx.navigation:navigation-compose:2.7.5")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,5 +56,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 }
--- app/src/main/java/com/luisperez/cuartitosa_app/presentation/viewModels/DashboardViewModel.kt
+++ app/src/main/java/com/luisperez/cuartitosa_app/presentation/viewModels/DashboardViewModel.kt
@@ -9,11 +9,11 @@ import com.luisperez.cuartitosa_app.data.Student
 
 class DashboardViewModel: ViewModel() {
     var students by mutableStateOf(listOf(
-        Student(1L, "Luffy", "Capitan pirata", "https://i.blogs.es/3c1231/luffy/1200_900.jpeg"),
-        Student(2L, "Zoro", "Espadachin", "https://i.redd.it/5k3h3zqxzrlf1.jpeg"),
-        Student(3L, "Chopper", "Medico del equipo", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdOUesRX9kv550KoFWy9vZ2b3mdgM-GWBVxg&s"),
-        Student(4L, "Ussopp", "Tirador del equipo", "https://static.wikia.nocookie.net/doblaje/images/7/78/OPFilm08_-_Usopp.png/revision/latest?cb=20240827003227&path-prefix=es"),
-        Student(5L, "Nami", "Negociante del equipo", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE1ZA7uCwrcmgQzHPTmM89tp80T_Cmwcn8lw&s"),
+        Student(1L, "Juan", "Portero", "https://i.blogs.es/3c1231/luffy/1200_900.jpeg"),
+        Student(2L, "Raul", "Defensa Central", "https://i.redd.it/5k3h3zqxzrlf1.jpeg"),
+        Student(3L, "Pedro", "Mediocentro Defensivo", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdOUesRX9kv550KoFWy9vZ2b3mdgM-GWBVxg&s"),
+        Student(4L, "Jose", "Medio punta", "https://static.wikia.nocookie.net/doblaje/images/7/78/OPFilm08_-_Usopp.png/revision/latest?cb=20240827003227&path-prefix=es"),
+        Student(5L, "Damian", "Delantero", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE1ZA7uCwrcmgQzHPTmM89tp80T_Cmwcn8lw&s"),
     ))
 
     fun addStudent( name: String, description: String, img: String){
